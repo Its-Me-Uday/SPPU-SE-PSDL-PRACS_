d5a8f41ad3dfa697253667235ae3c1bb3f67f69a
UDAY PRATAP SINGH <mera.naam.hai.uday4444@gmail.com>
2024-09-09 14:22:37 +0530

    Update and rename LED Interfacing

--- LED Interfacing and Blinking.cpp
+++ LED Interfacing and Blinking.cpp
@@ -3,7 +3,9 @@ void delay(unsigned int time)
 { 
    unsigned int i,j; 
     for(i=0;i<time;i++)
-     for(j=0;j<5000;j++); 
+      {
+        for(j=0;j<5000;j++);
+      }
 } 
 void main(void) 
 { 
